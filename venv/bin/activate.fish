5d53e9ace6ac3e328a70cfa57aa426dd4509ead3
samirsepahi <samirsepahi@packer.edu>
2023-04-04 21:07:33 -0400

    Preliminary LogIn + EventIt Name
    Jack had trouble accessing so done through Facetime. Pulling from Origin

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/smirsepahi/Documents/Code/microblog/venv"
+set -gx VIRTUAL_ENV "/Users/smirsepahi/Documents/Code/EventIt/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
